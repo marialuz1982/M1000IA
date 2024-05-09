cfe724d5ea4730630a94ddbd0859c956285b879c
gabriela <gafacrae@gmail.com>
2024-05-09 18:25:04 -0300

    estructura del proyecto
    agregue carpetas y archivos py

---
+++ IA FAKEXPERT/data/raw/data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Cargar los archivos CSV
+true_data = pd.read_csv(r'C:\Users\facus\OneDrive\Documents\M100O IA REPO\M1000IA\IA FAKEXPERT\data\raw\True_converted.csv')
+false_data = pd.read_csv(r'C:\Users\facus\OneDrive\Documents\M100O IA REPO\M1000IA\IA FAKEXPERT\data\raw\Fake_converted.csv')
+
+# Agregar una nueva columna indicando la fuente
+true_data['label'] = 'True'
+false_data['label'] = 'False'
+
+# Concatenar los DataFrames
+combined_data = pd.concat([true_data, false_data], ignore_index=True)
+
+# Guardar el nuevo dataset unificado en un CSV
+combined_data.to_csv(r'C:\Users\facus\OneDrive\Documents\M100O IA REPO\M1000IA\IA FAKEXPERT\data\processed\combined_data.csv', index=False)
